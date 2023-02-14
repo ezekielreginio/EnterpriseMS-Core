8f329241b0945e1bbd389d317da85cebd4f418cf
Sophia Mellea Dela Peña <sophiamellead@gmail.com>
2023-02-15 00:46:11 +0800

    added axios request on login form

--- resources/js/components/subcomponents/LoginComponent.vue
+++ resources/js/components/subcomponents/LoginComponent.vue
@@ -5,30 +5,47 @@
         <p class="fs-10 text-secondary">Log in with your data that you entered during your registration, or with the credentials provided by your organization administrator. </p>
 
         <div class="container p-0 m-0">
-            <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-            </div>
-            <div class="d-grid gap-2 mt-3">
-                <button class="btn primary-bg text-white py-3 btn-text" type="button">LOG IN</button>
-                <p class="text-primary cursor-pointer text-center pt-5 fs-5" @click.prevent="changeView('inquiry-form-component')">Inquire Today, Click Here!</p>
-            </div>
+            <form @submit.prevent="login">
+                <div class="form-floating mb-3">
+                    <input type="email" v-model="form.email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" v-model="form.password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="d-grid gap-2 mt-3">
+                    <button type="submit" class="btn primary-bg text-white py-3 btn-text">LOG IN</button>
+                    <p class="text-primary cursor-pointer text-center pt-5 fs-5" @click.prevent="changeView('inquiry-form-component')">Inquire Today, Click Here!</p>
+                </div>
+            </form>
         </div> 
     </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import UserService from '../services/UserService.js'
 
 export default {
+    data() {
+        return {
+            form: {
+                email: null,
+                password: null
+            } 
+        }
+    },
     methods: {
         ...mapActions({
             changeView: 'welcome/setCurrentView'
         }),
+        login() {
+            UserService.loginRequest(this.form)
+            .then((response) => {
+                console.log(response.data)
+            })
+        }
     }
 }
 </script>
